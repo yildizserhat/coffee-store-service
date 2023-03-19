5d908a3c37778bae913900f7873cb1fabb6bd2de
serhatyildiz <yildiz_serhat@hotmail.com>
2023-03-19 18:07:50 +0100

    Refactoring.

--- src/main/java/com/yildiz/serhat/coffeestoreservice/repository/UserRepository.java
+++ src/main/java/com/yildiz/serhat/coffeestoreservice/repository/UserRepository.java
@@ -11,7 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByToken(String token);
-
-    Optional<User> findByUsername(String username);
 }
 
